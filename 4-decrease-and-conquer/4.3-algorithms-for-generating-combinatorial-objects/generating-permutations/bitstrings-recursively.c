4959fc4287fea7543162d1c1a79db4e2fd663ab3
ayrton-hora <ayrtonfh@gmail.com>
2023-12-28 00:10:02 -0300

    add base for Bitstring recursively algorithm

---
+++ 4-decrease-and-conquer/4.3-algorithms-for-generating-combinatorial-objects/generating-permutations/bitstrings-recursively.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n = 0;
+    printf("BitString's Recursively Alghorithm to generate subsets on bitstring representation\n\n"); 
+    while (n <= 0) {
+        printf("Please insert a non-zero positive integer value: ");
+        scanf("%d", &n);
+    }
+    printf("\n");
+
+    printf("Subsets:\n");
+    // printf("\nTotal: %d\n\n", );
+
+    return 0;
+}
